ed4328e5575637887abd8d07d481c17265efa6ab
Stephen Burling <stephenburling99@gmail.com>
2020-09-09 03:33:51 +0000

    Uploaded code text from Visual Studio solution

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include "search.h"
 
+using namespace std;
+
 int main() {
   int list[] = {1,2,4,5,6,7,8,9};
   int array_length = 8;
-  int number = 3;
+  int number = 6;
   
-  std::cout << "Linear Search: " << LinearSearch(list,array_length,number) <<  std::endl;
+  cout << "Linear Search: " << LinearSearch(list,array_length,number) <<  endl;
 
-  std::cout << "Binar Search: " << BinarySearch(list,array_length,number) <<  std::endl;
+  cout << "Binary Search: " << BinarySearch(list,array_length,number) <<  endl;
 }
--- search.cpp
+++ search.cpp
@@ -1,13 +1,39 @@
-/****
-* PSEUDOCODE:
-*
-*/
-int LinearSearch(int numbers[], int length, int n) {
-    // TODO implement this function
-   return 1;
+int LinearSearch(int list[], int arrayLength, int number) {
+	
+	for (int i = 0; i < arrayLength; i++) {
+		if (number == list[i]) {
+			;
+			return i;
+		}
+	}
+	return -1;
 }
 
-int BinarySearch(int numbers[], int length, int n) {
-    // TODO implement this function
-     return 1;
+int BinarySearch(int list[], int arrayLength, int number)
+{
+	int ceilingNum = arrayLength - 1;
+	int floorNum = 0;
+
+	while (floorNum <= ceilingNum)
+	{
+		int midpoint = (floorNum + ceilingNum) / 2;
+
+		if (number == list[midpoint]) {
+
+			return midpoint;
+
+		}
+		else if (number < list[midpoint]) {
+
+			ceilingNum = midpoint - 1;
+
+		}
+		else { 
+
+			floorNum = midpoint + 1; 
+
+		}
+	}
+
+	return -1;
 }
