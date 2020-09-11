4817f34b08d9a42fdd62b487f42730a53ebcb85e
Stephen Burling <stephenburling99@gmail.com>
2020-09-11 01:46:15 +0000

    comments

--- search.cpp
+++ search.cpp
@@ -1,14 +1,22 @@
 /*
 LinearSearch(array, length of array, number to search)
-for every number starting from zero and to the end of the array: 
-  if the number to search for is in the index of the given array slot:
-  return the index of the number in the array.
-else, return -1.
+  for every number starting from zero and to the end of the array: 
+    if the number to search for is in the index of the given array slot:
+    return the index of the number in the array.
+  else, return -1.
 
 BinarySearch(array, length of array, number to search)
-Initilialize the ceiling bound to the length of the array minus one
-Initilialize the floor bound to 0
-While there are still
+  Initilialize the ceiling bound to the length of the array minus one
+  Initilialize the floor bound to 0
+  While the floor is less than or equal to the ceiling, that is while there are still elements to scan:
+    initialize the midpoint to the sum of the floor and ceiling divided in half
+    if the requested number is equal to the midpoint of the array:
+      return the midpoint
+    if the number is less than the midpoint:
+      set the ceilingNum to one less than the midpoint, disregarding the upper half
+    if the number is greater than the midpoint:
+      set the floorNum to one greater than the midpoint, disregarding the lower half
+  If the element is not present, return -1.
 */
 int LinearSearch(int list[], int arrayLength, int number) {
 	for (int i = 0; i < arrayLength; i++) {
